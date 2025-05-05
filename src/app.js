cc1b6ae1015c482d6ed27411428d9d686e3eec9c
igorfcfs <igorfcfs@gmail.com>
2025-05-05 13:56:36 -0300

    Liberação do Front com a Lib Cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 // app.js
 const express = require('express');
-const app = express();
+const cors = require('cors'); // Importe o módulo cors
 const dispositivosRoutes = require('./routes/dispositivosRoutes');
+
+const app = express();
+
+// Use o middleware cors *antes* de qualquer outra configuração de middleware ou rota
+app.use(cors());
 
 app.use(express.json());
 
+
+const corsOptions = {
+    origin: 'http://172.19.0.1:3001', // Substitua pelo domínio real do seu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Se você precisar de cookies ou headers de autenticação
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
 // Monta as rotas
-app.use('/', dispositivosRoutes); // Ou um prefixo como '/api'
+app.use('/', dispositivosRoutes);
 
 module.exports = app;
